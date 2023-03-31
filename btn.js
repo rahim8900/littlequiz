9ed2474e5a0e891e7f652012789264300593fc85
rahim8900 <rehimhesenov26@gmail.com>
2023-03-31 19:42:58 +0400

    second

--- btn.js
+++ btn.js
@@ -3,10 +3,14 @@ let lovhe = document.querySelector('.lovhe');
 let red = document.querySelector('.red');
 let blue = document.querySelector('.blue');
 red.addEventListener('click', fred);
-blue.addEventListener('mouseover', fblue);
+blue.addEventListener('mouseover', fblue1);
+blue.addEventListener('mouseout', fblue2)
 function fred(e) {
     return alert(`Elədə bilirdim!`);
 }
-function fblue(e) {
+function fblue1(e) {
     blue.style.display = 'none';
+}
+function fblue2(e) {
+    blue.style.display = 'block';
 }
